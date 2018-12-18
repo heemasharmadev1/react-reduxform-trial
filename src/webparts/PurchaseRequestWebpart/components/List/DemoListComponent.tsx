a2a3ae90f065fe695e12fb17b38665e47ee1efb1
Heema Sharma <HZS@baroda.sfwltd.co.uk>
2018-12-18 20:11:19 +0530

    changes to the new component

--- src/webparts/PurchaseRequestWebpart/components/List/DemoListComponent.tsx
+++ src/webparts/PurchaseRequestWebpart/components/List/DemoListComponent.tsx
@@ -12,6 +12,7 @@ import {
     DetailsList, DetailsListLayoutMode, Selection, SelectionMode, IColumn,
     MarqueeSelection
 } from "office-ui-fabric-react";
+import { ITrainingsItem } from "../../../../../lib/webparts/PurchaseRequestWebpart/state/INewTrainingControlsState";
 
 interface INewFormConnectedState {
     newFormControlValues: INewTrainingState;
@@ -21,74 +22,75 @@ interface INewFormConnectedDispatch {
     getAllTrainingItems(siteUrl): Promise<any>;
 }
 
-let _items: IDocument[] = [];
+let _items: ITrainingsItem[] = [];
 export interface IDetailsListDocumentsExampleState {
     columns: IColumn[];
-    items: IDocument[];
+    //items: IDocument[];
+    items: INewTrainingState;
     isModalSelection: boolean;
     isCompactMode: boolean;
 }
-export interface IDocument {
-    [key: string]: any;
-    name: string;
-    value: string;
-    iconName: string;
-    fileType: string;
-    modifiedBy: string;
-    dateModified: string;
-    dateModifiedValue: number;
-    fileSize: string;
-    fileSizeRaw: number;
-}
+// export interface IDocument {
+//     [key: string]: any;
+//     name: string;
+//     value: string;
+//     iconName: string;
+//     fileType: string;
+//     modifiedBy: string;
+//     dateModified: string;
+//     dateModifiedValue: number;
+//     fileSize: string;
+//     fileSizeRaw: number;
+// }
 
 class DemoListComponent extends React.Component<INewFormConnectedState & INewFormConnectedDispatch  & ITrainingWebpartProps & InjectedFormProps<{}, INewFormConnectedState>, IDetailsListDocumentsExampleState>{
     constructor(props) {
         super(props);
         //  Populate with items for demos.
         if (_items.length === 0) {
-            for (let i = 0; i < 10; i++) {
+            // for (let i = 0; i < 10; i++) {
                 
-                let fileName =  "Test 12";
-                let userName  =  "Lorem Ipsum";
-                //fileName = fileName.charAt(0).toUpperCase() + fileName.slice(1).concat(`.${randomFileType.docType}`);
+            //     let fileName =  "Test 12";
+            //     let userName  =  "Lorem Ipsum";
+            //     //fileName = fileName.charAt(0).toUpperCase() + fileName.slice(1).concat(`.${randomFileType.docType}`);
 
-                _items.push({
-                    name: fileName,
-                    value: fileName,
-                    iconName: "",
-                    fileType: "",
-                    modifiedBy: userName,
-                    dateModified: "",
-                    dateModifiedValue: 12,
-                    fileSize: "",
-                    fileSizeRaw: 20
-                });
-            }
+            //     _items.push({
+            //         name: fileName,
+            //         value: fileName,
+            //         iconName: "",
+            //         fileType: "",
+            //         modifiedBy: userName,
+            //         dateModified: "",
+            //         dateModifiedValue: 12,
+            //         fileSize: "",
+            //         fileSizeRaw: 20
+            //     });
+            // }
             //  _items = this._sortItems(_items, 'name');
+
         }
 
-
         const _columns: IColumn[] = [
+            // {
+            //     key: 'column1',
+            //     name: 'File Type',
+            //     headerClassName: 'DetailsListExample-header--FileIcon',
+            //     className: 'DetailsListExample-cell--FileIcon',
+            //     iconClassName: 'DetailsListExample-Header-FileTypeIcon',
+            //     ariaLabel: 'Column operations for File type, Press to sort on File type',
+            //     iconName: 'Page',
+            //     isIconOnly: true,
+            //     fieldName: 'name',
+            //     minWidth: 16,
+            //     maxWidth: 16,
+            //    // onColumnClick: this._onColumnClick,
+            //     onRender: (item: IDocument) => {
+            //         return <img src={item.iconName} className={'DetailsListExample-documentIconImage'} alt={item.fileType + ' file icon'} />;
+            //     }
+            // },
             {
                 key: 'column1',
-                name: 'File Type',
-                headerClassName: 'DetailsListExample-header--FileIcon',
-                className: 'DetailsListExample-cell--FileIcon',
-                iconClassName: 'DetailsListExample-Header-FileTypeIcon',
-                ariaLabel: 'Column operations for File type, Press to sort on File type',
-                iconName: 'Page',
-                isIconOnly: true,
-                fieldName: 'name',
-                minWidth: 16,
-                maxWidth: 16,
-               // onColumnClick: this._onColumnClick,
-                onRender: (item: IDocument) => {
-                    return <img src={item.iconName} className={'DetailsListExample-documentIconImage'} alt={item.fileType + ' file icon'} />;
-                }
-            },
-            {
-                key: 'column2',
-                name: 'Name',
+                name: 'Training Name',
                 fieldName: 'name',
                 minWidth: 210,
                 maxWidth: 350,
@@ -103,22 +105,22 @@ class DemoListComponent extends React.Component<INewFormConnectedState & INewFor
                 isPadded: true
             },
             {
-                key: 'column3',
-                name: 'Date Modified',
+                key: 'column2',
+                name: 'Date of Training',
                 fieldName: 'dateModifiedValue',
                 minWidth: 70,
                 maxWidth: 90,
                 isResizable: true,
                // onColumnClick: this._onColumnClick,
                 data: 'number',
-                onRender: (item: IDocument) => {
-                    return <span>{item.dateModified}</span>;
-                },
+                // onRender: (item: IDocument) => {
+                //     return <span>{item.dateModified}</span>;
+                // },
                 isPadded: true
             },
             {
-                key: 'column4',
-                name: 'Modified By',
+                key: 'column3',
+                name: 'Training Status',
                 fieldName: 'modifiedBy',
                 minWidth: 70,
                 maxWidth: 90,
@@ -126,25 +128,26 @@ class DemoListComponent extends React.Component<INewFormConnectedState & INewFor
                // isCollapsible: true,
                 data: 'string',
                // onColumnClick: this._onColumnClick,
-                onRender: (item: IDocument) => {
-                    return <span>{item.modifiedBy}</span>;
-                },
+                // onRender: (item: IDocument) => {
+                //     return <span>{item.modifiedBy}</span>;
+                // },
                 isPadded: true
-            },
-            {
-                key: 'column5',
-                name: 'File Size',
-                fieldName: 'fileSizeRaw',
-                minWidth: 70,
-                maxWidth: 90,
-                isResizable: true,
-               // isCollapsible: true,
-                data: 'number',
-               // onColumnClick: this._onColumnClick,
-                onRender: (item: IDocument) => {
-                    return <span>{item.fileSize}</span>;
-                }
             }
+            //,
+            // {
+            //     key: 'column5',
+            //     name: 'File Size',
+            //     fieldName: 'fileSizeRaw',
+            //     minWidth: 70,
+            //     maxWidth: 90,
+            //     isResizable: true,
+            //    // isCollapsible: true,
+            //     data: 'number',
+            //    // onColumnClick: this._onColumnClick,
+            //     onRender: (item: IDocument) => {
+            //         return <span>{item.fileSize}</span>;
+            //     }
+            // }
         ];
 
         
@@ -152,6 +155,7 @@ class DemoListComponent extends React.Component<INewFormConnectedState & INewFor
       
           this.state = {
             items: _items,
+            //items: ,
             columns: _columns,
             isModalSelection: false,
             isCompactMode: false
